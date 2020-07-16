c850b5cde67d7b920979f5a52bc36aa968aa4ff2
Raj Verma <info.mrverma@gmail.com>
2020-07-16 19:55:22 +0530

    upload image

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -5,12 +5,21 @@ import {Router} from '@angular/router';
 import {AuthService} from '../../services/auth.service';
 import {NgForm} from '@angular/forms';
 
+import { finalize } from 'rxjs/operators';
+
 // firebase
 import { AngularFireStorage } from '@angular/fire/storage';
 import { AngularFireDatabase } from '@angular/fire/database';
 
+import { readAndCompressImage } from 'browser-image-resizer';
 
-
+const config = {
+  quality: 0.5,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true,
+  debug: true
+};
 
 @Component({
   selector: 'app-signup',
@@ -20,6 +29,8 @@ import { AngularFireDatabase } from '@angular/fire/database';
 export class SignupComponent implements OnInit {
 
   picture : string = "https://images.pexels.com/photos/2681319/pexels-photo-2681319.jpeg";
+
+  uploadPercent : number = null;
 
   constructor(
     private auth : AuthService,
@@ -62,6 +73,31 @@ export class SignupComponent implements OnInit {
 
   }
 
+  async uploadFile(event){
+      console.log(event);
+      const file = event.target.files[0];
 
+      let resizedImage = await readAndCompressImage(file,config);
+
+      const filePath = file.name;
+      const fileRef = this.storage.ref(filePath);
+
+      const task = this.storage.upload(filePath,resizedImage);
+
+      task.percentageChanges().subscribe((percentage)=>{
+          this.uploadPercent = percentage;
+      });
+
+      task.snapshotChanges().pipe(
+        finalize(()=>{
+          fileRef.getDownloadURL().subscribe((url)=>{
+            this.picture = url;
+            this.toastr.success("image upload successfully...");
+          })
+        })
+      ).subscribe();
+
+
+  }
 
 }
