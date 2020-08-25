3f482134db011e57646bfab990b7a248713479e7
Raj Verma <info.mrverma@gmail.com>
2020-08-25 19:50:25 +0530

    project complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,23 +6,36 @@ import {SignupComponent} from './pages/signup/signup.component';
 import {AddpostComponent} from './pages/addpost/addpost.component';
 import {HomeComponent} from './pages/home/home.component';
 
+import { AngularFireAuthGuard,redirectUnauthorizedTo,redirectLoggedInTo } from '@angular/fire/auth-guard';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(["signin"]);
+const redirectLoggedInToHome = () => redirectLoggedInTo([""])
 
 const routes: Routes = [
   {
     path: '',
-    component : HomeComponent
+    component : HomeComponent,
+    canActivate : [AngularFireAuthGuard],
+    data : { authGuardPipe : redirectUnauthorizedToLogin }
   },
   {
     path: 'signin',
-    component : SigninComponent
+    component : SigninComponent,
+    canActivate : [AngularFireAuthGuard],
+    data : { authGuardPipe : redirectLoggedInToHome }
+
   },
   {
     path: 'signup',
-    component : SignupComponent
+    component : SignupComponent,
+    canActivate : [AngularFireAuthGuard],
+    data : { authGuardPipe : redirectLoggedInToHome }
   },
   {
     path: 'addpost',
-    component: AddpostComponent
+    component: AddpostComponent,
+    canActivate : [AngularFireAuthGuard],
+    data : { authGuardPipe : redirectUnauthorizedToLogin }
   }
 ];
 
